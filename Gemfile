909bdb06e6087c258a8726794a44855d0deb4ac7
Joel Lim <akaru@littlix.com>
2019-07-08 09:13:42 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'require_all'
+gem 'rake'
+gem 'shotgun'
+gem 'rspec'
+gem 'rack-test'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require_relative '../config/environment'
+
+require 'rack/test'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  config.order = 'default'
+end
+
+def app
+  Rack::Builder.parse_file('config.ru').first
+end
